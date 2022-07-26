3b1c3c94f095e3f887970f50d2f4a58e9d0441fa
shaquielle robbins <shaquiellerobbins@gmail.com>
2022-07-26 12:15:09 -0500

    Movie Page - added buttons, styled buttons in css, added to nav bar, added to router, added FA to html

--- js/views/About.js
+++ js/views/About.js
@@ -1,18 +1,52 @@
 import {showNotification} from "../messaging.js";
+import {getUser} from "../auth.js";
 
-export default function About(props) {
-    // language=HTML
+
+export default function MoviesHTMLFunction(props) {
     return `
-<header>
-    <h1>About Page</h1>
-</header>
-<main>
-    <div>
-        <p>About page is here.</p>  
-    </div>
-</main>
+        <header>
+            <h1 class="text-center">Movies</h1>
+        </header>
+        <a data-link href="/add-movie" target="_blank" <button class="btn"> <i class="fa fa-plus"></i></button></a>
+        
+        <main>
+            <div id="movieListContainer">
+            ${makeMovieCards(props.movies)} 
+            </div>
+        </main>
     `;
+
+    function makeMovieCards(movies) {
+        let html = "";
+        movies.forEach(function (movie){
+            html += makeMovieCard(movie)
+        });
+        return html;
+    }
+
+    function makeMovieCard (movie){
+        return `
+    <div class="card">
+        <div class="cardBody">
+            <p>${movie.title}</p>
+            <button class="btn"><i class="fa fa-pencil"></i></button>
+            <button class="btn"><i class="fa fa-trash"></i></button>
+        </div>
+    </div>`
+
+    }
 }
-export function AboutEvents() {
-    showNotification("Hey, a message!", "danger");
+
+
+export function MoviesJSFunction() {
+    const user = getUser();
+    if(!user) {
+        showNotification("Welcome visitor", "secondary");
+    } else {
+        showNotification("Welcome " + user.userName, "info");
+    }
 }
+
+
+
+
