73dbecdfe8e58a69a3ff7693c9f9765f12bead03
Chase Forestello <101914667+Chase-Forestello@users.noreply.github.com>
2022-07-27 13:48:19 -0500

    working on edit button

--- js/views/About.js
+++ js/views/About.js
@@ -59,11 +59,14 @@ export default function MoviesHTMLFunction(props) {
 
     function makeMovieCard(movie) {
         return `
-    <div class="card col-3">
-        <div class="cardBody">
-            <p>${movie.title}</p>
-            <button type="button" class="btn btn-primary" data-bs-toggle="modal" data-bs-target="#editMovieModal"><i class="fa fa-pencil"></i></button>
-        
+    <div class="card col-3 h-100">
+      <img src="assets/jalopy1.jpeg" class="card-img-top" alt="...">
+      <div class="card-body">
+        <h5 class="card-title text-center">${movie.title}</h5>
+        <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content.</p>
+      </div>
+      <div id="movieFoot">
+        <button type="button" class="btn btn-primary editBtn" data-id="${movie.id}" data-bs-toggle="modal" data-bs-target="#editMovieModal"><i class="fa fa-pencil"></i></button>
         <!-- Modal -->
         <div class="modal fade" id="editMovieModal" tabindex="-1" aria-labelledby="editMovieModalLabel" aria-hidden="true">
           <div class="modal-dialog">
@@ -76,11 +79,11 @@ export default function MoviesHTMLFunction(props) {
                     <h1>Edit Movie</h1>
                     <form>
                         <label for="editMovieTitle" class="form-label">Title:</label>
-                        <input class="form-control text-center" list="datalistOptions" id="editMovieTitle" placeholder="Enter new movie title">
+                        <input class="form-control text-center" list="datalistOptions" id="editMovieTitle">
                         <label for="editMovieDirector" class="form-label">Director:</label>
-                        <input class="form-control text-center" list="datalistOptions" id="editMovieDirector" placeholder="Enter the Director's name">
+                        <input class="form-control text-center" list="datalistOptions" id="editMovieDirector">
                         <label for="editMovieRating" class="form-label">Rating:</label>
-                        <input class="form-control text-center" list="datalistOptions" id="editMovieRating" placeholder="Enter a movie rating 0-5">
+                        <input class="form-control text-center" list="datalistOptions" id="editMovieRating">
                     </form>
                     <button class="form-control btn insert-btn mt-3" data-bs-dismiss="modal" id="editMovieBtn">Save Movie</button>
                 </div>
@@ -93,13 +96,43 @@ export default function MoviesHTMLFunction(props) {
         </div>
             <button class="delBtn btn" data-id="${movie.id}"><i class="fa fa-trash"></i></button>
         </div>
-    </div>`
+      </div>
+`
 
     }
 }
 
 
 export function MoviesJSFunction() {
+
+    let editButton = document.getElementsByClassName(`editBtn`);
+    for (let i = 0; i < editButton.length; i++) {
+        editButton[i].addEventListener("click", getMovieData)
+    }
+    async function getMovieData () {
+        const requestOptions = {
+            method: "GET",
+        }
+        const id = this.getAttribute(`data-id`)
+        const getMovieData = await fetch(`https://glory-cedar-barge.glitch.me/movies/${id}`, requestOptions)
+            .then(async function (response) {
+                if (!response.ok) {
+                    console.log("add movie error: " + response.status);
+                } else {
+                    console.log("add movie ok");
+                    return await response.json();
+                }
+            });
+        let titleValue = document.getElementById(`editMovieTitle`);
+        titleValue.setAttribute("value", `${getMovieData.title}`)
+        let directorValue = document.getElementById(`editMovieDirector`);
+        directorValue.setAttribute("value", `${getMovieData.director}`)
+        let ratingValue = document.getElementById(`editMovieRating`);
+        ratingValue.setAttribute("value", `${getMovieData.rating}`)
+        console.log(getMovieData.title);
+        console.log(getMovieData.director);
+        console.log(getMovieData.rating);
+    }
 
     let delButton = document.getElementsByClassName('delBtn');
     for (let i = 0; i < delButton.length; i++) {
