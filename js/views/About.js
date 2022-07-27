4986a11882b3ea93f68a04e124daf4fdbefa7981
Chase Forestello <101914667+Chase-Forestello@users.noreply.github.com>
2022-07-27 10:34:34 -0500

    Working on movie disaply and modal for editing movies

--- js/views/About.js
+++ js/views/About.js
@@ -8,10 +8,42 @@ export default function MoviesHTMLFunction(props) {
         <header>
             <h1 class="text-center">Movies</h1>
         </header>
-        <a id="addMoviePlusBtn" data-link href="/add-a-movie" target="_blank">+</a>
+<!--        <a id="addMoviePlusBtn" data-link href="/add-a-movie" target="_blank">+</a>-->
+        <!-- Button trigger modal -->
+        <button type="button" class="btn btn-primary" data-bs-toggle="modal" data-bs-target="#exampleModal">
+          +
+        </button>
+        
+        <!-- Modal -->
+        <div class="modal fade" id="exampleModal" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+          <div class="modal-dialog">
+            <div class="modal-content">
+              <div class="modal-header">
+                <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+              </div>
+              <div class="modal-body">
+                <div class="container text-center">
+                    <h1>New Movie</h1>
+                    <form>
+                        <label for="newMovieTitle" class="form-label">Title:</label>
+                        <input class="form-control text-center" list="datalistOptions" id="newMovieTitle" placeholder="Enter new movie title">
+                        <label for="newMovieDirector" class="form-label">Director:</label>
+                        <input class="form-control text-center" list="datalistOptions" id="newMovieDirector" placeholder="Enter the Director's name">
+                        <label for="newMovieRating" class="form-label">Rating:</label>
+                        <input class="form-control text-center" list="datalistOptions" id="newMovieRating" placeholder="Enter a movie rating 0-5">
+                    </form>
+                    <button class="form-control btn insert-btn mt-3" data-bs-dismiss="modal" id="addMovieBtn">Add Movie</button>
+                </div>
+              </div>
+              <div class="modal-footer">
+                <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
+              </div>
+            </div>
+          </div>
+        </div>
         
         <main>
-            <div id="movieListContainer">
+            <div id="movieListContainer" class="row">
             ${makeMovieCards(props.movies)} 
             </div>
         </main>
@@ -19,18 +51,46 @@ export default function MoviesHTMLFunction(props) {
 
     function makeMovieCards(movies) {
         let html = "";
-        movies.forEach(function (movie){
+        movies.forEach(function (movie) {
             html += makeMovieCard(movie)
         });
         return html;
     }
 
-    function makeMovieCard (movie){
+    function makeMovieCard(movie) {
         return `
-    <div class="card">
+    <div class="card col-3">
         <div class="cardBody">
             <p>${movie.title}</p>
-            <button class="btn"><i class="fa fa-pencil"></i></button>
+            <button type="button" class="btn btn-primary" data-bs-toggle="modal" data-bs-target="#editMovieModal"><i class="fa fa-pencil"></i></button>
+        
+        <!-- Modal -->
+        <div class="modal fade" id="editMovieModal" tabindex="-1" aria-labelledby="editMovieModalLabel" aria-hidden="true">
+          <div class="modal-dialog">
+            <div class="modal-content">
+              <div class="modal-header">
+                <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+              </div>
+              <div class="modal-body">
+                <div class="container text-center">
+                    <h1>Edit Movie</h1>
+                    <form>
+                        <label for="editMovieTitle" class="form-label">Title:</label>
+                        <input class="form-control text-center" list="datalistOptions" id="editMovieTitle" placeholder="Enter new movie title">
+                        <label for="editMovieDirector" class="form-label">Director:</label>
+                        <input class="form-control text-center" list="datalistOptions" id="editMovieDirector" placeholder="Enter the Director's name">
+                        <label for="editMovieRating" class="form-label">Rating:</label>
+                        <input class="form-control text-center" list="datalistOptions" id="editMovieRating" placeholder="Enter a movie rating 0-5">
+                    </form>
+                    <button class="form-control btn insert-btn mt-3" data-bs-dismiss="modal" id="editMovieBtn">Save Movie</button>
+                </div>
+              </div>
+              <div class="modal-footer">
+                <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
+              </div>
+            </div>
+          </div>
+        </div>
             <button class="delBtn btn" data-id="${movie.id}"><i class="fa fa-trash"></i></button>
         </div>
     </div>`
@@ -63,12 +123,47 @@ export function MoviesJSFunction() {
         }
     }
 
-    const user = getUser();
-    if(!user) {
-        showNotification("Welcome visitor", "secondary");
-    } else {
-        showNotification("Welcome " + user.userName, "info");
+    const insertMovieBtn = document.querySelector("#addMovieBtn");
+    insertMovieBtn.addEventListener("click", addMovie)}
+
+function addMovie() {
+    // make sure user entered something non-blank for the dog fact
+    const newMovieTitleInput = document.getElementById(`newMovieTitle`);
+    const newMovieDirectorInput = document.getElementById(`newMovieDirector`);
+    const newMovieRatingInput = document.getElementById(`newMovieRating`);
+    const newMovieTitle = newMovieTitleInput.value.trim();
+    const newMovieDirector = newMovieDirectorInput.value.trim();
+    const newMovieRating = newMovieRatingInput.value.trim();
+    const factInput = document.querySelector("#dogFactText");
+    if(newMovieTitle.length < 1 || newMovieDirector.length < 1 || newMovieRating.length === null) {
+        alert("Entries cannot be blank!")
+        console.log("Entries cannot be blank!");
+        return;
     }
+    const newMovie = {
+        title: newMovieTitle,
+        director: newMovieDirector,
+        rating: newMovieRating,
+    };
+
+    console.log("Movie is ready to be inserted");
+
+    const requestOptions = {
+        method: "POST",
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify(newMovie)
+    }
+    fetch("https://glory-cedar-barge.glitch.me/movies", requestOptions)
+        .then(function(response) {
+            if(!response.ok) {
+                console.log("add movie error: " + response.status);
+            } else {
+                console.log("add movie ok");
+                createView("/about");
+            }
+        });
 }
 
 
--- js/views/add-a-movie.js
+++ js/views/add-a-movie.js
@@ -22,45 +22,45 @@ export default function AddAMovieHTMLFunction(props) {
 }
 
 export function AddAMovieJSFunction() {
-    const insertMovieBtn = document.querySelector("#addMovieBtn");
-    insertMovieBtn.addEventListener("click", addMovie)}
-
-function addMovie() {
-    // make sure user entered something non-blank for the dog fact
-    const newMovieTitleInput = document.getElementById(`newMovieTitle`);
-    const newMovieDirectorInput = document.getElementById(`newMovieDirector`);
-    const newMovieRatingInput = document.getElementById(`newMovieRating`);
-    const newMovieTitle = newMovieTitleInput.value.trim();
-    const newMovieDirector = newMovieDirectorInput.value.trim();
-    const newMovieRating = newMovieRatingInput.value.trim();
-    const factInput = document.querySelector("#dogFactText");
-    if(newMovieTitle.length < 1 || newMovieDirector.length < 1 || newMovieRating.length === null) {
-        alert("Entries cannot be blank!")
-        console.log("Entries cannot be blank!");
-        return;
-    }
-    const newMovie = {
-            title: newMovieTitle,
-            director: newMovieDirector,
-            rating: newMovieRating,
-        };
-
-    console.log("Movie is ready to be inserted");
-
-    const requestOptions = {
-        method: "POST",
-        headers: {
-            'Content-Type': 'application/json'
-        },
-        body: JSON.stringify(newMovie)
-    }
-    fetch("https://glory-cedar-barge.glitch.me/movies", requestOptions)
-        .then(function(response) {
-            if(!response.ok) {
-                console.log("add movie error: " + response.status);
-            } else {
-                console.log("add movie ok");
-                createView("/about");
-            }
-        });
+//     const insertMovieBtn = document.querySelector("#addMovieBtn");
+//     insertMovieBtn.addEventListener("click", addMovie)}
+//
+// function addMovie() {
+//     // make sure user entered something non-blank for the dog fact
+//     const newMovieTitleInput = document.getElementById(`newMovieTitle`);
+//     const newMovieDirectorInput = document.getElementById(`newMovieDirector`);
+//     const newMovieRatingInput = document.getElementById(`newMovieRating`);
+//     const newMovieTitle = newMovieTitleInput.value.trim();
+//     const newMovieDirector = newMovieDirectorInput.value.trim();
+//     const newMovieRating = newMovieRatingInput.value.trim();
+//     const factInput = document.querySelector("#dogFactText");
+//     if(newMovieTitle.length < 1 || newMovieDirector.length < 1 || newMovieRating.length === null) {
+//         alert("Entries cannot be blank!")
+//         console.log("Entries cannot be blank!");
+//         return;
+//     }
+//     const newMovie = {
+//             title: newMovieTitle,
+//             director: newMovieDirector,
+//             rating: newMovieRating,
+//         };
+//
+//     console.log("Movie is ready to be inserted");
+//
+//     const requestOptions = {
+//         method: "POST",
+//         headers: {
+//             'Content-Type': 'application/json'
+//         },
+//         body: JSON.stringify(newMovie)
+//     }
+//     fetch("https://glory-cedar-barge.glitch.me/movies", requestOptions)
+//         .then(function(response) {
+//             if(!response.ok) {
+//                 console.log("add movie error: " + response.status);
+//             } else {
+//                 console.log("add movie ok");
+//                 createView("/about");
+//             }
+//         });
 }
